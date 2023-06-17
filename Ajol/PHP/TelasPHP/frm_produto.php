f873c3b499d6e4c3965fb17d9826effd9226f2e4
LuizEduardoRodrigues <Luizeduardot.i@outlook.com>
2023-06-16 22:16:47 -0300

    frm cadastro produto

--- Ajol/PHP/TelasPHP/frm_produto.php
+++ Ajol/PHP/TelasPHP/frm_produto.php
@@ -1,15 +1,98 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../js/bootstrap.bundle.js">
-    <title>Document</title>
+    <title>Cadastro Produto</title>
 </head>
-<body>
-        
 
+<body>
+    <form action="" method="post" class="form-control">
+        <div class="row mt-3 ">
+            <div class="col-sm-6">Nome do Produto
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+            </div>
+            <div class="col-sm-3 mt-4">
+                <input type="date" class="form-control" style="border-radius: 25px;" name="txtCadastro" id="txtCadastro">
+            </div>
+            <div class="col-sm-2"> ID do Produto
+                <input type="number" class="form-control" style="border-radius: 25px;" name="txtID" min="0">
+            </div>
+            <div class="col-sm-1 text-center mt-4">
+                <button class="btn btn-primary" style="border-radius: 25px;" name="btoPesquisa" id="btoPesquisa" formaction="_sistema.php?tela=produto">&#128269;</button>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4 mt-4 card">
+                Imagem produto
+            </div>
+            <div class="col-sm-8">
+                <div class="row mt-3">
+                    <div class="col-sm-4">Categoria
+                        <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                    </div>
+                    <div class="col-sm-4">Marca
+                        <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                    </div>
+                    <div class="col-sm-4">Quantidade
+                        <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                    </div>
+                </div>
+                <div class="row mt-3 ">
+                    <div class="col-sm-4">Valor unitario
+                        <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                    </div>
+                    <div class="col-sm-4">Valor custo
+                        <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                    </div>
+                    <div class="col-sm-4">Percentual de lucro %
+                        <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3 ">
+            <div class="col-sm-5">
+                <input type="file" class="btn btn-primary btn-lg btn-block">
+            </div>
+        </div>
+                        <div class="row mt-3 float-sm-end">
+                            <div class="col-sm-3">Altura (cm)
+                                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                            </div>
+                            <div class="col-sm-3">Largura (cm)
+                                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                            </div>
+                            <div class="col-sm-3">Comprimento (cm)
+                                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                            </div>
+                            <div class="col-sm-3">Peso (kg)
+                                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome">
+                            </div>
+                        </div>
+                        
+
+                 <div class="row mt-3 ">
+                    <div class="col-sm-4">
+
+                    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+                      </div>
+                    <div class="col-sm-4">
+                    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+                    </div>
+                <div class="col-sm-4">
+                    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+                </div>
+            </div>
+                     
+            
+    </form>
 </body>
+
 </html>
