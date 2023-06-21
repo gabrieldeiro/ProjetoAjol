97db85b83d7cc8e1009d4308f8cb9cc3667301e9
silvtop <emfiso@hotmail.com>
2023-06-21 20:09:24 -0300

    frm func - linkado

--- Ajol/PHP/TelasPHP/frm_funcionario.php
+++ Ajol/PHP/TelasPHP/frm_funcionario.php
@@ -11,16 +11,16 @@
 
 <?php include_once('../ConexaoPHP/funcionario_pesquisa.php') ?>
 
-
+<!-- os que estao no value estao na variavel de funcionario_pesquisa -->
 <body>
     <form action="" method="post" class="form-control" style="background-color: darkgray;"enctype="multipart/form-data">
         <div class="row mt-3 ">
             <div class="col-sm-2"> ID do Usuario
-                <input type="text" class="form-control" style="border-radius: 25px;" name="txtId" id="txtId" value="<?$nome?>" >
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtId" id="txtId" value="<?=$idFuncionario?>" >
             </div>
         
         <div class="col-sm-6">Nome do Usuario
-                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome" value="<?$nome?>">
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome" value="<?= $nomeFuncionario?>">
             </div>
                     
             <div class="col-sm-1 text-center mt-4">
@@ -32,63 +32,63 @@
 
         <div class="row mt-3 ">
             <div class="col-sm-4">Cpf
-                <input type="text" class="form-control" style="border-radius: 25px;" name="txtCpf" id="txtCpf" value="<?$cpf?>">
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtCpf" id="txtCpf" value="<?=$cpfFuncionario?>">
             </div>
             <div class="col-sm-4"> Data de Nascimento
-                <input type="date" class="form-control" style="border-radius: 25px;" name="txtNascimento" id="txtNascimento" value="<?$datanascimento?>">
+                <input type="date" class="form-control" style="border-radius: 25px;" name="txtNascimento" id="txtNascimento" value="<?=$datanascimentoFuncionario?>">
             </div>
         
              <div class="col-mt-5">Foto funcionario
-                <input type="file" class="form-control"  name="txtImg" id="txtImg" value="<?$img?>">
+                <input type="file" class="form-control"  name="txtImg" id="txtImg" value="<?=$img?>">
 
              </div>
         </div>
         <div class="row mt-3 ">
             <div class="col-sm-4">Login
-            <input type="text" class="form-control" style="border-radius: 25px;" name="txtLogin" id="txtLogin" value="<?$login?>">
+            <input type="text" class="form-control" style="border-radius: 25px;" name="txtLogin" id="txtLogin" value="<?=$loginFuncionario?>">
             </div>
             <div class="col-sm-4"> Senha
-                <input type="password" class="form-control" style="border-radius: 25px;" name="txtSenha" id="txtSenha" value="<?$senha?>">
+                <input type="password" class="form-control" style="border-radius: 25px;" name="txtSenha" id="txtSenha" value="<?=$senhaFuncionario?>">
             </div>
             
         </div>
 
         <div class="row mt-3 ">
             <div class="col-sm-4">Endereço
-            <input type="text" class="form-control" style="border-radius: 25px;" name="txtEndereco" id="txtEndereco" value="<?$endereco?>">
+            <input type="text" class="form-control" style="border-radius: 25px;" name="txtEndereco" id="txtEndereco" value="<?=$enderecoFuncionario?>">
             </div>
             <div class="col-sm-2"> Numero
-                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNumero" id="txtNumero" value="<?$numero?>">
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNumero" id="txtNumero" value="<?=$numeroFuncionario?>">
             </div>
 
             <div class="col-sm-2">Complemento
-            <input type="text" class="form-control" style="border-radius: 25px;" name="txtComplemento" id="txtComplemento">
+            <input type="text" class="form-control" style="border-radius: 25px;" name="txtComplemento" id="txtComplemento" value="<?=$complementoFuncionario?>">
             </div>
             <div class="col-sm-2">Bairro
-                <input type="text" class="form-control" style="border-radius: 25px;" name="txtBairro" id="txtBairro">
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtBairro" id="txtBairro" value="<?=$bairroFuncionario?>">
            
             
         </div>
              <div class="col-sm-2">CEP
-            <input type="text" class="form-control" style="border-radius: 25px;" name="txtCep" id="txtCep">
+            <input type="text" class="form-control" style="border-radius: 25px;" name="txtCep" id="txtCep" value="<?=$cepFuncionario?>">
             </div>
             
             <div class="col-sm-4">Status
                     <select name="txtStatus" class="form-control">
                         <option value="">--Selecione um Status--</option>
-                        <option value="ativo" <?= ($status == 'ativo' ? 'selected' : "") ?>>Ativo</option>
-                        <option value="inativo" <?= ($status == 'inativo' ? 'selected' : "") ?>>Inativo</option>
+                        <option value="ativo" <?= ($statusFuncionario == 'ativo' ? 'selected' : "") ?>>Ativo</option>
+                        <option value="inativo" <?= ($statusFuncionario == 'inativo' ? 'selected' : "") ?>>Inativo</option>
                     </select>
                 </div>
 
                 <div class="col-sm-4">Email
-            <input type="email" class="form-control" style="border-radius: 25px;" name="txtEmail" id="txtEmail">
+            <input type="email" class="form-control" style="border-radius: 25px;" name="txtEmail" id="txtEmail" value="<?=$emailFuncionario?>">
             </div>
 
                 <div class="row mt-3">
                 <div class="col-sm-12">
                     <textarea class="form-control" name="txtObs" id="txtObs" rows="5"
-                        placeholder="Observação "></textarea>
+                        placeholder="Observação " value="<?=$obsFuncionario?>"></textarea>
                 </div>
             </div>
             
