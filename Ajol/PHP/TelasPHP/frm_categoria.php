1cbd49a0be60687087f5898419f8693d31a945b6
silvtop <emfiso@hotmail.com>
2023-06-20 21:21:28 -0300

    tela categoria concluida e testada

--- Ajol/PHP/TelasPHP/frm_categoria.php
+++ Ajol/PHP/TelasPHP/frm_categoria.php
@@ -13,7 +13,7 @@
 <body>
     
 
-<!-- <?php include_once('.php') ?>pesquisar -->
+<?php include_once('../ConexaoPHP/categoria_pesquisa.php') ?>
     <div class="container-fluid   mt-3">
         <div class="row mt-3">
             <div class="col-sm-12">
@@ -24,24 +24,25 @@
         <form action="" method="post"  class="form-control " style="background-color: darkgray;">
         <div class="row mt-3">
                 <div class="col-sm-4">
-                    <input type="text" class="form-control"  style="border-radius: 25px;" name="txtidcategoria"
-                        placeholder="ID categoria">
+                    <input type="text" class="form-control"  style="border-radius: 25px;" name="txtId"
+                        placeholder="ID categoria" value="<?=$idCategoria?>">
+                        <!-- este id esta no categoria pesquisa -->
                 </div>
 
             <div class="row mt-3">
                 <div class="col-sm-4">
                     <input type="text" class="form-control"  style="border-radius: 25px;" name="txtNome"
-                        placeholder="nome categoria">
+                        placeholder="nome categoria"value="<?=$nomeCategoria?>">
                 </div>
-                <!-- <div class="col-sm-4">
+                <div class="col-sm-4">
                     <button class="btn btn-primary" name="btopesquisar" id="btopesquisar"
-                        formaction="_sistema.php?tela=filme">&#128269;</button> -->
-                <!-- </div> --> pesquisar
+                        formaction="frm_categoria.php">&#128269;</button> 
+              
             </div>
            
             <div class="row mt-3">
                 <div class="col-sm-4">
-                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtDescricao"  placeholder="descricao categoria">
+                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtDescricao"  placeholder="descricao categoria" value="<?=$descricaoCategoria?>">
 
                   </div>
             
@@ -50,8 +51,8 @@
                 <div class="col-sm-4">
                     <select name="txtStatus" class="form-control">
                         <option value="">--Selecione um Status--</option>
-                        <option value="ativo" <?= ($status == 'ativo' ? 'selected' : "") ?>>Ativo</option>
-                        <option value="inativo" <?= ($status == 'inativo' ? 'selected' : "") ?>>Inativo</option>
+                        <option value="ativo" <?= ($statusCategoria == 'ativo' ? 'selected' : "") ?>>Ativo</option>
+                        <option value="inativo" <?= ($statusCategoria == 'inativo' ? 'selected' : "") ?>>Inativo</option>
                     </select>
                 </div>
             </div>
@@ -59,16 +60,17 @@
             <div class="row mt-3">
                 <div class="col-sm-12">
                     <textarea class="form-control" name="txtObs" id="txtObs" rows="5"
-                        placeholder="Observação "></textarea>
+                        placeholder="Observação " ><?=$obsCategoria?></textarea>
                 </div>
             </div>
             
             <div class="row mt-3">
                 <div class="col-sm-12 text-end">
-                    <button name="btoCadastrar"  class="btn btn-primary"formaction="../ConexaoPHP/categoria_cadastrar.php">Cadastrar</button>
-                    <button name="btoAlterar"  class="btn btn-danger"formaction="../ConexaoPHP/categoria_alterar.php">Alterar</button>
-                    <button name="btoLimpar" id="btoLimpar" class="btn btn-secundary" >Limpar</button><a href="categoria_cadastrar.php"></a>
-                    <button name="btoExcluir"  class="btn btn-warning" formaction="frm_categoria.php">Sair</button>
+                    <button name="btoCadastrar"  class="btn btn-success"formaction="../ConexaoPHP/categoria_cadastrar.php">Cadastrar</button>
+                    <button name="btoAlterar"  class="btn btn-warning"formaction="../ConexaoPHP/categoria_alterar.php">Alterar</button>
+                    <button name="btoLimpar" id="btoLimpar" class="btn btn-primary" >Limpar</button><a href="categoria_cadastrar.php"></a>
+                    <button name="btoExcluir"  class="btn btn-danger"formaction="../ConexaoPHP/categoria_excluir.php">Excluir</button>
+                   
                 </div>
             </div>
         </form>
