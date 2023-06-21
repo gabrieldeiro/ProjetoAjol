bf458cea883f74e0fbd967702853882062e90087
jrgabriel28 <gabrieldeiro2018@gmail.com>
2023-06-21 19:53:06 -0300

    produto PHP finalizado

--- Ajol/PHP/ConexaoPHP/produto_pesquisa.php
+++ Ajol/PHP/ConexaoPHP/produto_pesquisa.php
@@ -3,6 +3,7 @@
 
     $idProduto = '';
     $idcategoriaProduto = '';
+    $nomecategoriaProduto = '';
     $nomeProduto = '';
     $marcaProduto = '';
     $datacadastroProduto = '';
@@ -19,7 +20,41 @@
     $statusProduto = '';
 
 
-    if($)
+    if($_POST)
+    {
+        $id = $_POST['txtId'];
+
+        $sql = $conn->query('select produto.*, categoria.nome_categoria from produto inner join categoria on categoria.id_categoria = produto.id_categoria_produto WHERE produto.id_produto ='.$id);
+
+        if($sql->rowCount()>=1)
+        {
+            foreach ($sql as $row) 
+            {
+                $idProduto=$row[0];
+                $idcategoriaProduto=$row[1];
+                $nomecategoriaProduto=$row[16];
+                $nomeProduto=$row[2];
+                $marcaProduto=$row[3];
+                $datacadastroProduto=$row[4];
+                $loteProduto=$row[5];
+                $qtdeProduto=$row[6];
+                $pesoProduto=$row[7];
+                $escalaProduto=$row[8];
+                $valorProduto=$row[9];
+                $descontoProduto=$row[10];
+                $imgProduto=$row[11];
+                $img2Produto=$row[14];
+                $img3Produto=$row[15];
+                $obsProduto=$row[12];
+                $statusProduto=$row[13];
+            }
+
+        }
+        else
+        {
+            echo '<p>Produto não encontrado</p>';
+        }
+    }
 
 ?>
 
