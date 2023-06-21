f591e1e50b72b3702eb107ff80c0226ac85ef1c9
GustavoTosta <tgustavo@outlook.com>
2023-06-21 19:05:59 -0300

    Update frm_categoria.php

--- Ajol/PHP/TelasPHP/frm_categoria.php
+++ Ajol/PHP/TelasPHP/frm_categoria.php
@@ -1,20 +1,4 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../js/bootstrap.bundle.js">
-   
-
-</head>
-<body>
-    
-
 <?php include_once('../ConexaoPHP/categoria_pesquisa.php') ?>
-    <div class="container-fluid   mt-3">
         <div class="row mt-3">
             <div class="col-sm-12">
                 <h1>Categoria</h1>
@@ -74,7 +58,3 @@
                 </div>
             </div>
         </form>
-    </div>
-     
-</body>
-</html> 
