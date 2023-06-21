7304c14336564bab3d674057959da02d0bc4986f
jrgabriel28 <gabrieldeiro2018@gmail.com>
2023-06-20 22:14:22 -0300

    atts

---
+++ Ajol/PHP/ConexaoPHP/produto_pesquisa.php
@@ -0,0 +1,25 @@
+<?php
+    include_once('conexao')
+
+
+
+
+?>
+
+
+
+$idcategoria = $_POST['txtIdcategoria'];
+$nome = $_POST['txtNome'];
+$marca = $_POST['txtMatca'];
+$datacadastro = $_POST['txtData'];
+        $lote = $_POST['txtLote'];
+        $qtde = $_POST['txtQtde'];
+        $peso = $_POST['txtPeso'];
+        $escala = $_POST['txtEscala'];
+        $valor = $_POST['txtValor'];
+        $desconto = $_POST['txtDesconto'];
+        $img = '';
+        $img2 = '';
+        $img3 = '';
+        $obs = $_POST['txtObs'];
+        $status = $_POST['txtStatus'];
