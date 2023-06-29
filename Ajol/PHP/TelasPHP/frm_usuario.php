002a5bc5399ca73a7fa7ac07ad95525a66ba40fe
LuizEduardoRodrigues <Luizeduardot.i@outlook.com>
2023-06-28 22:15:35 -0300

    atualização

--- Ajol/PHP/TelasPHP/frm_usuario.php
+++ Ajol/PHP/TelasPHP/frm_usuario.php
@@ -4,82 +4,83 @@
         resize: none !important;
     }
 </style>
-        <form action="" method="post" enctype="multipart/form-data" style="background-color: darkgray;">
+<form action="" method="post" enctype="multipart/form-data" style="background-color: darkgray;">
 
-            <center>
-                    <div class="card border border-danger" style="width: 325px; height: 204px;">
-                        <img src="../../img/usu/<?=$idUsuario?>/<?=$imgUsuario?>" class="w-100 img-fluid"  alt="">
+    <center>
+        <div class="card border border-danger" style="width: 325px; height: 204px;">
+            <img src="../../img/usu/<?= $idUsuario ?>/<?= $imgUsuario ?>" class="w-100 img-fluid" alt="">
 
-                    </div>
-                    <div class="card-body">
-                        <input type="file" class="btn btn-primary " name="txtImg">
-                    </div>
-            </center>
+        </div>
+        <div class="card-body">
+            <input type="file" class="btn btn-primary " name="txtImg">
+        </div>
+    </center>
+
+    <hr>
+
+        <div class="row mt-3 ">
+            <div class="col-sm-6">Nome do Usuario
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome" value="<?= $nomeUsuario ?>">
+            </div>
+            <div class="col-sm-3">CPF
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtCpf" id="txtCpf" value="<?= $cpfUsuario ?>">
+            </div>
+            <div class="col-sm-2"> ID do Usuario
+                <input type="number" class="form-control" style="border-radius: 25px;" name="txtId" min="0" value="<?= $idUsuario ?>">
+            </div>
+            <div class="col-sm-1 text-center mt-4">
+                <button class="btn btn-primary" style="border-radius: 25px;" name="btoPesquisa" id="btoPesquisa" formaction="sistema.php?tela=usuario">&#128269;</button>
+            </div>
+
         
-            <hr> 
-
-            <div class="row mt-3 ">
-                <div class="col-sm-6">Nome do Usuario
-                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtNome" id="txtNome" value="<?= $nomeUsuario ?>">
-                </div>
-                <div class="col-sm-3 mt-4">
-                    <input type="date" class="form-control" style="border-radius: 25px;" name="txtCadastro" id="txtCadastro" hidden>
-                </div>
-                <div class="col-sm-2"> ID do Usuario
-                    <input type="number" class="form-control" style="border-radius: 25px;" name="txtId" min="0" value="<?= $idUsuario ?>">
-                </div>
-                <div class="col-sm-1 text-center mt-4">
-                    <button class="btn btn-primary" style="border-radius: 25px;" name="btoPesquisa" id="btoPesquisa" formaction="sistema.php?tela=usuario">&#128269;</button>
-                </div>
-
-            </div>
-
-            <div class="row mt-3 ">
-                <div class="col-sm-3">Login
-                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtLogin" id="txtLogin" value="<?= $loginUsuario ?>">
-                </div>
-                <div class="col-sm-3"> Senha
-                    <input type="password" class="form-control" style="border-radius: 25px;" name="txtSenha" id="txtSenha" value="<?= $senhaUsuario ?>">
-                </div>
-                <div class="col-sm-3"> Data de Nascimento
-                    <input type="date" class="form-control" style="border-radius: 25px;" name="txtNascimento" id="txtNascimento" value="<?= substr($datanascimentoUsuario, 0, 10) ?>">
-                </div>
-                <div class="col-sm-3">CPF
-                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtCpf" id="txtCpf" value="<?= $cpfUsuario ?>">
-                </div>
-
-            </div>
-
-            <div class="row mt-3 ">
+            <div class="row mt-2 ">
                 <div class="col-sm-6">Email
                     <input type="text" class="form-control" style="border-radius: 25px;" name="txtEmail" id="txtEmail" value="<?= $emailUsuario ?>">
                 </div>
 
+                <div class="col-sm-3"> Data de Nascimento
+                    <input type="date" class="form-control" style="border-radius: 25px;" name="txtNascimento" id="txtNascimento" value="<?= substr($datanascimentoUsuario, 0, 10) ?>">
+                </div>
+            <hr>
+        </div>
 
-            <div class="row mt-3 ">
-                <div class="col-sm-3">CEP
-                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtCep" id="txtCep" value="<?= $cepUsuario ?>">
-                </div>
-                <div class="col-sm-5">Endereço
-                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtEndereco" id="txtEndereco" value="<?= $enderecoUsuario ?>">
-                </div>
-                <div class="col-sm-2">Bairro
-                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtBairro" id="txtBairro" value="<?= $enderecoUsuario ?>">
-                </div>
-                <div class="col-sm-1">Numero
-                    <input type="text" class="form-control" style="border-radius: 25px;" name="txtNumero" id="txtNumero" value="<?= $numeroUsuario ?>">
-                </div>
-                <div class="col-sm-5">Complemento
-                    <textarea name="txtComplemento" class="form-control" id="txtComplemento" style="border-radius: 25px;" cols="15" rows="2"></textarea
-                            value="<?= $complementoUsuario ?>"> 
-                    </div>
-                  
-                    <div class="col-sm-4"> Observação
-                        <textarea class="form-control textarea1" name="txtObs" id="txtObs" rows="1" placeholder="Observação " value="<?= $obsProduto ?>"></textarea>
-                </div>
+        <div class="row mt-3 ">
+        <div class="col-sm-3">CEP
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtCep" id="txtCep" value="<?= $cepUsuario ?>">
+            </div>
+            <div class="col-sm-5">Endereço
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtEndereco" id="txtEndereco" value="<?= $enderecoUsuario ?>">
+            </div>
+            <div class="col-sm-2">Numero
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtNumero" id="txtNumero" value="<?= $numeroUsuario ?>">
             </div>
 
-                <div class="row mt-3">
+            <div class="col-sm-2">Bairro
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtBairro" id="txtBairro" value="<?= $enderecoUsuario ?>">
+            </div>
+            <div class="col-sm-3">Estado
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtEstado" id="txtEstado" value="<?= $estadoUsuario ?>">
+            </div>
+            <div class="col-sm-3">Cidade
+                <input type="text" class="form-control" style="border-radius: 25px;" name="txtCidade" id="txtCidade" value="<?= $cidadeUsuario ?>">
+            </div>
+            <div class="col-sm-5">Complemento
+                <textarea name="txtComplemento" class="form-control textarea1" id="txtComplemento" style="border-radius: 25px;" cols="15" rows="1"></textarea
+                        value="<?= $complementoUsuario ?>"> 
+               </div>
+                
+               <hr>
+            </div>
+
+    
+
+                <div class="row mt-4">
+                    <div class="col-sm-3">Login
+                        <input type="text" class="form-control" style="border-radius: 25px;" name="txtLogin" id="txtLogin" value="<?= $loginUsuario ?>">
+                     </div>
+                     <div class="col-sm-3"> Senha
+                         <input type="password" class="form-control" style="border-radius: 25px;" name="txtSenha" id="txtSenha" value="<?= $senhaUsuario ?>">
+                    </div>
                     <div class="col-sm-3">Status
                         <select name="txtStatus" id="textStatus" class="form-control" style="border-radius: 25px;">
                             <option value="">--Selecione um Status--</option>
@@ -88,18 +89,22 @@
                             <option value="inativo" <?= ($statusUsuario == 'Inativo' ? 'selected' : "") ?>>Inativo
                             </option>
                         </select>
-                    </div>
-                </div>
-
-        
-
-
-            <div class="row mt-3">
-                <div class="col-sm-12 text-end">
-                    <button name="btoexcluir" id="btoexcluir" class="btn btn-danger btn-lg" formaction="usuario_excluir.php">Excluir</button>
-                    <a href="../TelasPHP/sistema.php?tela=usuario" name="btoLimpar" id="btoLimpar" class="btn btn-primary btn-lg">Limpar</a>
-                    <button id="btnalterar" name="btnalterar" class="btn btn-warning btn-lg" formaction="../ConexaoPHP/usuario_alterar.php">Alterar</button>
-                    <button name="btocadastrar" id="btocadastrar" class="btn btn-success btn-lg" formaction="../ConexaoPHP/usuario_cadastrar.php">Cadastrar</button>
-                </div>
+                    </div>  
+                    <div class="col-sm-4"> Observação
+                        <textarea class="form-control textarea1" name="txtObs" id="txtObs" rows="2" placeholder="Observação " value="<?= $obsProduto ?>"></textarea>
             </div>
-    </form>
+
+        </div>
+
+
+
+
+        <div class="row mt-3">
+            <div class="col-sm-12 text-end">
+                <button name="btoexcluir" id="btoexcluir" class="btn btn-danger btn-lg" formaction="usuario_excluir.php">Excluir</button>
+                <a href="../TelasPHP/sistema.php?tela=usuario" name="btoLimpar" id="btoLimpar" class="btn btn-primary btn-lg">Limpar</a>
+                <button id="btnalterar" name="btnalterar" class="btn btn-warning btn-lg" formaction="../ConexaoPHP/usuario_alterar.php">Alterar</button>
+                <button name="btocadastrar" id="btocadastrar" class="btn btn-success btn-lg" formaction="../ConexaoPHP/usuario_cadastrar.php">Cadastrar</button>
+            </div>
+        </div>
+</form>
