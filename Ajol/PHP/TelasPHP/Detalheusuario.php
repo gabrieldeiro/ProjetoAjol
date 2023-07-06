b0fcc1188d738a21e7423f9e4c44dfd4d8c4ecc6
LuizEduardoRodrigues <Luizeduardot.i@outlook.com>
2023-07-05 22:15:38 -0300

    atualização

---
+++ Ajol/PHP/TelasPHP/Detalheusuario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../js/bootstrap.bundle.js">
+    <!---->
+    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="//assets.locaweb.com.br/locastyle/2.0.6/stylesheets/locastyle.css">
+   <!---->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.0.3.min.js"></script>
+    <!---->
+    <script type="text/javascript" src="//assets.locaweb.com.br/locastyle/2.0.6/javascripts/locastyle.js"></script>
+    <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post" style="background-color: darkgray;" enctype="multipart/form-data">
+    
+    <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6 text-center"> Usuario
+        <input type="email" class="form-control form-control-lg" id="colFormLabelLg" placeholder="Usuario">
+        </div>
+        <div class="col-sm-3"></div>
+    </div>
+    <div class="row mt-1">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-3 text-center"> Login
+         <input type="email" class="form-control form-control-lg" id="colFormLabelLg" placeholder="Login">
+        </div>
+        <div class="col-sm-3 text-center"> Senha
+            <input type="Text" class="form-control form-control-lg" id="colFormLabelLg" placeholder="Senha">
+        </div>
+    </div>
+</form>
+</body>
+</html>
