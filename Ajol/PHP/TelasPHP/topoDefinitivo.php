d98ad7fccace19ac396dad64d24543abe09fcfb0
GustavoTosta <tgustavo@outlook.com>
2023-07-20 22:12:25 -0300

    aaaaaaaaaaaaaaaaaaa

--- Ajol/PHP/TelasPHP/topoDefinitivo.php
+++ Ajol/PHP/TelasPHP/topoDefinitivo.php
@@ -29,39 +29,12 @@
     vertical-align: middle;
     text-align: center;    
     }
-<<<<<<< HEAD
 
     .perfil{
         width: 70px;
         height: 50px;
         object-fit: cover;
         object-position: 50% 0%;
-=======
-    .css-rainbow-text {
-        background: linear-gradient(90deg, #f00, #ff2b00, #f50, #ff8000, #fa0, #ffd500, #ff0, #d4ff00, #af0, #80ff00, #5f0, #2bff00, #0f0, #00ff2b, #0f5, #00ff80, #0fa, #00ffd5, #0ff, #00d4ff, #0af, #007fff, #05f, #002bff, #00f, #2a00ff, #50f, #7f00ff, #a0f, #d400ff, #f0f, #ff00d4, #f0a, #ff0080, #f05, #ff002b, #f00);
-        color: #0000;
-        -webkit-background-clip: text;
-        background-clip: text;
-        font-weight: bold;
-        text-decoration: none;
-    }
-    .rainbow
-    {
-        text-decoration: none;
-        background: linear-gradient(90deg, #f00, #ff2b00, #f50, #ff8000, #fa0, #ffd500, #ff0, #d4ff00, #af0, #80ff00, #5f0, #2bff00, #0f0, #00ff2b, #0f5, #00ff80, #0fa, #00ffd5, #0ff, #00d4ff, #0af, #007fff, #05f, #002bff, #00f, #2a00ff, #50f, #7f00ff, #a0f, #d400ff, #f0f, #ff00d4, #f0a, #ff0080, #f05, #ff002b, #f00);
-    }
-    .chapeu
-    {
-        position: absolute;
-        right: 100px;
-        top: -25px;
-    }
-    .chapeu2
-    {
-        position: absolute;
-        right: 135px;
-        top: -50px;
->>>>>>> 6c8e6c335ed4e14dc3e997d7193129a9fdb940e2
     }
     .balao1
     {
